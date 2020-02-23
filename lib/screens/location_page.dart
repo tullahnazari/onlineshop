d0b6f14fedfad6600164ed63339d0f19b09038d1
tnaz <tullah.nazari@gmail.com>
2020-02-22 18:56:38 -0600

    Created static map in location page

--- lib/screens/location_page.dart
+++ lib/screens/location_page.dart
@@ -1,30 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
 import 'package:provider/provider.dart';
+import 'package:sweepstakes/helper/location_helper.dart';
 import 'package:sweepstakes/models/user_location.dart';
 import 'package:sweepstakes/providers/location_service.dart';
 
-class LocationPage extends StatelessWidget {
+class LocationPage extends StatefulWidget {
   static const routeName = '/location-page';
 
-  const LocationPage({Key key}) : super(key: key);
+  @override
+  _LocationPageState createState() => _LocationPageState();
+}
+
+class _LocationPageState extends State<LocationPage> {
+  String _previewImageUrl;
+
+  Future<void> getCurrentLocation() async {
+    final location = await Location().getLocation();
+    final staticMapImageUrl = LocationHelper.generateLocationPreviewImage(
+      latitude: location.latitude,
+      longitude: location.longitude,
+    );
+    setState(() {
+      _previewImageUrl = staticMapImageUrl;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    Future<void> getCurrentLocation() async {
-      final location = await Location().getLocation();
-      print(location.latitude);
-      print(location.longitude);
-    }
-
     // getLocation.getLocation();
     return Scaffold(
       body: Center(
-        child: RaisedButton(
-          child: Text('get location'),
-          onPressed: () {
-            getCurrentLocation();
-          },
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              height: 200,
+            ),
+            Container(
+              height: 400,
+              width: 350,
+              child: _previewImageUrl == null
+                  ? Text(
+                      'No location chosen',
+                      textAlign: TextAlign.center,
+                    )
+                  : Image.network(
+                      _previewImageUrl,
+                      fit: BoxFit.cover,
+                      width: double.infinity,
+                    ),
+            ),
+            RaisedButton(
+              child: Text('Find Location'),
+              onPressed: () {
+                getCurrentLocation();
+              },
+            ),
+          ],
         ),
       ),
     );
