ce17c6819a8e01eb0c37c7f95f9a560583283bd8
tnaz <tullah.nazari@gmail.com>
2020-05-08 10:21:05 -0500

    iOS isssues solved

--- lib/screens/auth-screen.dart
+++ lib/screens/auth-screen.dart
@@ -163,7 +163,8 @@ class AuthCard extends StatefulWidget {
   _AuthCardState createState() => _AuthCardState();
 }
 
-class _AuthCardState extends State<AuthCard> {
+class _AuthCardState extends State<AuthCard>
+    with SingleTickerProviderStateMixin {
   bool checkTheBox = false;
   final GlobalKey<FormState> _formKey = GlobalKey();
   AuthMode _authMode = AuthMode.Login;
@@ -173,6 +174,31 @@ class _AuthCardState extends State<AuthCard> {
   };
   var _isLoading = false;
   final _passwordController = TextEditingController();
+  AnimationController _controller;
+  Animation<Size> _heightAnimation;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 300),
+    );
+    _heightAnimation = Tween<Size>(
+            begin: Size(double.infinity, 310), end: Size(double.infinity, 370))
+        .animate(
+      CurvedAnimation(parent: _controller, curve: Curves.fastOutSlowIn),
+    );
+    _heightAnimation.addListener(() => setState(() {}));
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _controller.dispose();
+  }
 
   void _showErrorDialog(String message) {
     showDialog(
@@ -197,9 +223,9 @@ class _AuthCardState extends State<AuthCard> {
       // Invalid!
       return;
     }
-    if (checkTheBox == false) {
-      return _showErrorDialog('Please agree to the terms before registering.');
-    }
+    // if (checkTheBox == false) {
+    //   return _showErrorDialog('Please agree to the terms before registering.');
+    // }
     _formKey.currentState.save();
     setState(() {
       _isLoading = true;
@@ -247,10 +273,12 @@ class _AuthCardState extends State<AuthCard> {
       setState(() {
         _authMode = AuthMode.Signup;
       });
+      _controller.forward();
     } else {
       setState(() {
         _authMode = AuthMode.Login;
       });
+      _controller.reverse();
     }
   }
 
@@ -258,128 +286,137 @@ class _AuthCardState extends State<AuthCard> {
   Widget build(BuildContext context) {
     final deviceSize = MediaQuery.of(context).size;
     return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      elevation: 8.0,
-      child: Container(
-        height: _authMode == AuthMode.Signup ? 370 : 310,
-        constraints:
-            BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 370 : 310),
-        width: deviceSize.width * 0.90,
-        padding: EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-                TextFormField(
-                  decoration: InputDecoration(labelText: 'E-Mail'),
-                  keyboardType: TextInputType.emailAddress,
-                  validator: (value) {
-                    if (value.isEmpty || !value.contains('@')) {
-                      return 'Invalid email!';
-                    }
-                  },
-                  onSaved: (value) {
-                    _authData['email'] = value;
-                  },
-                ),
-                TextFormField(
-                  decoration: InputDecoration(labelText: 'Password'),
-                  obscureText: true,
-                  controller: _passwordController,
-                  validator: (value) {
-                    if (value.isEmpty || value.length < 5) {
-                      return 'Password is too short!';
-                    }
-                  },
-                  onSaved: (value) {
-                    _authData['password'] = value;
-                  },
-                ),
-                if (_authMode == AuthMode.Signup)
-                  Column(
-                    children: <Widget>[
-                      TextFormField(
-                        enabled: _authMode == AuthMode.Signup,
-                        decoration:
-                            InputDecoration(labelText: 'Confirm Password'),
-                        obscureText: true,
-                        validator: _authMode == AuthMode.Signup
-                            ? (value) {
-                                if (value != _passwordController.text) {
-                                  return 'Passwords do not match!';
-                                }
-                              }
-                            : null,
-                      ),
-                      Row(
-                        children: <Widget>[
-                          Checkbox(
-                            activeColor: Theme.of(context).primaryColor,
-                            value: checkTheBox,
-                            onChanged: (bool value) {
-                              setState(() {
-                                checkTheBox = value;
-                              });
-                            },
-                          ),
-                          Text('By clicking Register, you agree to our'),
-                        ],
-                      ),
-                      InkWell(
-                        onTap: () {
-                          Navigator.of(context).pushNamed(EULA.routeName);
-                        },
-                        child: new Text(
-                          'Terms and Conditions.',
-                          style:
-                              TextStyle(color: Theme.of(context).primaryColor),
-                        ),
-                      ),
-                    ],
-                  ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        elevation: 8.0,
+        child: AnimatedContainer(
+          duration: Duration(milliseconds: 300),
+          curve: Curves.easeInOut,
 
-                // RaisedButton(
-                //   child: Text('EULA'),
-                //   onPressed: () {
-                //     Navigator.of(context).pushNamed(EULA.routeName);
-                //   },
-                // ),
-                SizedBox(
-                  height: 20,
-                ),
-                if (_isLoading)
-                  CircularProgressIndicator(
-                    backgroundColor: Theme.of(context).primaryColor,
-                  )
-                else
-                  RaisedButton(
-                    child: Text(
-                        _authMode == AuthMode.Login ? 'LOGIN' : 'REGISTER'),
-                    onPressed: _submit,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
+          //height: _heightAnimation.value.height,
+
+          height: _authMode == AuthMode.Signup ? 370 : 310,
+          constraints: BoxConstraints(
+              minHeight: _authMode == AuthMode.Signup ? 370 : 310),
+          width: deviceSize.width * 0.90,
+          padding: EdgeInsets.all(16.0),
+
+          child: Form(
+            key: _formKey,
+            child: SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  TextFormField(
+                    decoration: InputDecoration(labelText: 'E-Mail'),
+                    keyboardType: TextInputType.emailAddress,
+                    validator: (value) {
+                      if (value.isEmpty || !value.contains('@')) {
+                        return 'Invalid email!';
+                      }
+                    },
+                    onSaved: (value) {
+                      _authData['email'] = value;
+                    },
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(labelText: 'Password'),
+                    obscureText: true,
+                    controller: _passwordController,
+                    validator: (value) {
+                      if (value.isEmpty || value.length < 5) {
+                        return 'Password is too short!';
+                      }
+                    },
+                    onSaved: (value) {
+                      _authData['password'] = value;
+                    },
+                  ),
+                  if (_authMode == AuthMode.Signup)
+                    Column(
+                      children: <Widget>[
+                        TextFormField(
+                          enabled: _authMode == AuthMode.Signup,
+                          decoration:
+                              InputDecoration(labelText: 'Confirm Password'),
+                          obscureText: true,
+                          validator: _authMode == AuthMode.Signup
+                              ? (value) {
+                                  if (value != _passwordController.text) {
+                                    return 'Passwords do not match!';
+                                  }
+                                  if (checkTheBox == false) {
+                                    return 'Please agree to the terms before registering.';
+                                  }
+                                }
+                              : null,
+                        ),
+                        Row(
+                          children: <Widget>[
+                            Checkbox(
+                              activeColor: Theme.of(context).primaryColor,
+                              value: checkTheBox,
+                              onChanged: (bool value) {
+                                setState(() {
+                                  checkTheBox = value;
+                                });
+                              },
+                            ),
+                            Text('By clicking Register, you agree to our'),
+                          ],
+                        ),
+                        InkWell(
+                          onTap: () {
+                            Navigator.of(context).pushNamed(EULA.routeName);
+                          },
+                          child: new Text(
+                            'Terms and Conditions.',
+                            style: TextStyle(
+                                color: Theme.of(context).primaryColor),
+                          ),
+                        ),
+                      ],
                     ),
+
+                  // RaisedButton(
+                  //   child: Text('EULA'),
+                  //   onPressed: () {
+                  //     Navigator.of(context).pushNamed(EULA.routeName);
+                  //   },
+                  // ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  if (_isLoading)
+                    CircularProgressIndicator(
+                      backgroundColor: Theme.of(context).primaryColor,
+                    )
+                  else
+                    RaisedButton(
+                      child: Text(
+                          _authMode == AuthMode.Login ? 'LOGIN' : 'REGISTER'),
+                      onPressed: _submit,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 30.0, vertical: 8.0),
+                      color: Theme.of(context).accentColor,
+                      textColor: Theme.of(context).primaryColor,
+                    ),
+                  FlatButton(
+                    child: Text(
+                        '${_authMode == AuthMode.Login ? 'REGISTER' : 'LOGIN'} INSTEAD'),
+                    onPressed: _switchAuthMode,
                     padding:
-                        EdgeInsets.symmetric(horizontal: 30.0, vertical: 8.0),
-                    color: Theme.of(context).accentColor,
+                        EdgeInsets.symmetric(horizontal: 30.0, vertical: 4),
+                    materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
                     textColor: Theme.of(context).primaryColor,
                   ),
-                FlatButton(
-                  child: Text(
-                      '${_authMode == AuthMode.Login ? 'REGISTER' : 'LOGIN'} INSTEAD'),
-                  onPressed: _switchAuthMode,
-                  padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 4),
-                  materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                  textColor: Theme.of(context).primaryColor,
-                ),
-              ],
+                ],
+              ),
             ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
